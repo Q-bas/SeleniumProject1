667bc89cb91d2a3a2bb4579b78563cbf9b9997e6
Andrew <kubyk.a@gmail.com>
2022-06-13 22:07:48 -0500

    login page1

---
+++ src/main/java/pageObjects/LoginPage.java
@@ -0,0 +1,29 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+    public WebDriver driver;
+
+    By emailFld = By.id("user_email");
+    By pwdFld = By.id("user_password");
+    By sgnBtn = By.xpath("//input[@name='commit']");
+
+    public LoginPage(WebDriver driver){
+        this.driver=driver;
+    }
+    public WebElement emailField() {
+        return driver.findElement(emailFld);
+    }
+
+    public WebElement passwordField(){
+        return driver.findElement(pwdFld);
+    }
+
+    public WebElement signinButton(){
+        return driver.findElement(sgnBtn);
+    }
+
+}
--- src/test/java/academy/HomePage.java
+++ src/test/java/academy/HomePage.java
@@ -1,14 +1,15 @@
 package academy;
 
 import java.io.IOException;
-import java.time.Duration;
+// import java.time.Duration;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.support.ui.ExpectedConditions;
+// import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
+import pageObjects.LoginPage;
 
 public class HomePage extends Base{
 
@@ -17,11 +18,15 @@ public class HomePage extends Base{
         driver=initializeDriver();
         driver.get("http://qaclickacademy.com/");
         LandingPage lp = new LandingPage(driver);
+        LoginPage lip = new LoginPage(driver);
         
         // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         // wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("sumome-react-wysiwyg-move-handle")));
         lp.closePopUp().click();
         lp.logIn().click();
+        lip.emailField().sendKeys("email@m.mail");
+        lip.passwordField().sendKeys("pwd");
+        lip.signinButton().click();
     }
     
 }
